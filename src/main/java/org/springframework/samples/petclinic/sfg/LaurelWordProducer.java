c66940b143fd416f97c5ad6e710e8365da2ed43c
Camilo Romero <militoromero10@gmail.com>
2020-11-19 16:30:46 -0500

    test with spring context

---
+++ src/main/java/org/springframework/samples/petclinic/sfg/LaurelWordProducer.java
@@ -0,0 +1,15 @@
+package org.springframework.samples.petclinic.sfg;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+public class LaurelWordProducer implements WordProducer {
+
+    @Override
+    public String getWord() {
+        return "Laurel";
+    }
+}
+
