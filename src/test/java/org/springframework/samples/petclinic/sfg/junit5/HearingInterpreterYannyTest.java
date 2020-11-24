7321281b5cca693f67445e52dbac3795c3cbd08b
= <=>
2020-11-24 12:55:16 -0500

    junit5 context configure yanny

---
+++ src/test/java/org/springframework/samples/petclinic/sfg/junit5/HearingInterpreterYannyTest.java
@@ -0,0 +1,22 @@
+package org.springframework.samples.petclinic.sfg.junit5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.sfg.BaseConfig;
+import org.springframework.samples.petclinic.sfg.HearingInterpreter;
+import org.springframework.samples.petclinic.sfg.LaurelConfig;
+import org.springframework.samples.petclinic.sfg.YannyConfig;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+@SpringJUnitConfig(classes = {BaseConfig.class, YannyConfig.class})
+class HearingInterpreterYannyTest {
+
+    @Autowired
+    HearingInterpreter hearingInterpreter;
+
+    @Test
+    void whatIHeard() {
+        String value = hearingInterpreter.whatIHeard();
+        assert value.equals("Yanny");
+    }
+}
